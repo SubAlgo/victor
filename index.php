76557d171ffbc7ad483ec854414d97ad673d4778
korn <kornchaval@gmail.com>
2018-04-11 20:57:48 +0700

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Victor solution-HOME</title>
+</head>
+<body>
+    <?php
+        include_once './src/component/topheader.html';
+        include_once './src/component/navbar.php';
+    ?>
+</body>
+</html>
---
+++ src/component/navbar.php
@@ -0,0 +1,6 @@
+<div class="navbar" style="position: relative;  display: inline-block;">
+    <?php
+        include_once "logo.html";
+        include_once "menu.html";
+    ?>
+</div>
