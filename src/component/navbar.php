e3978a82a9b936ca16a86a6b22b35c4e8d1f2f3e
korn <kornchaval@gmail.com>
2018-04-15 21:21:52 +0700

    start edit navbar

--- src/component/navbar.php
+++ src/component/navbar.php
@@ -1,6 +1,18 @@
 <div class="navbar-container">
+
+    <div class="navbar-logo-container">
+        <img class="navbar-logo" src="./src/img/rectangular.png" alt="">
+    </div>
+
+    <div class="navbar-menu-container">
+        <a href="" class="navbar-menu">HOME</a>
+        <a href="" class="navbar-menu">ABOUT US</a>
+        <a href="" class="navbar-menu">SERVICE</a>
+        <a href="" class="navbar-menu">CONTACT US</a>
+    </div>
+
     <?php
-        include_once "logo.html";
-        include_once "menu.html";
+        //include_once "logo.html";
+        //include_once "menu.html";
     ?>
 </div>
